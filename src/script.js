1a89e0ad4476e505746079cbea244a0723c8a642
luctst <luc.tostee@outlook.fr>
2018-08-23 01:54:58 +0200

    Add sass-loader, webpack dev server

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 const App = document.querySelector("#app");
 const h1 = document.createElement("h1");
+const Name = "Lucas";
 
-h1.textContent = "Hello webpack";
+h1.textContent = `Hello ${Name}`;
 App.appendChild(h1);
