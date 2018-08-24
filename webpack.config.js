2f4f626443f0ec1c831bc6b9d4483dbd78d923b0
lucas tostée <lucas.tostee@gmail.com>
2018-08-24 11:08:37 +0200

    Add sass-loader, webpack dev server (#12)

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,27 @@
 const CleanWebpack = require("clean-webpack-plugin");
+const WebpackHtml = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: "./src/script.js",
     output: {
         filename: "main.js"
     },
+    devServer: {
+        contentBase: "dist/"
+    },
     plugins: [
+        new WebpackHtml( {
+            template: "./src/index.html"
+        } ),
         new CleanWebpack( ['dist'] ),
     ],
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ["style-loader","css-loader"]
-            },
+                test: /\main.scss/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            }
         ],
     }
 };
