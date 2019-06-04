fa974cc3d25a8e31e9270179460c5cc5ccb64cf1
Lucas Tostée <lucas.tostee@gmail.com>
2019-06-04 19:50:23 -0400

    Start creating projects page

--- src/container/ProjectHandler/ProjectHandler.js
+++ src/container/ProjectHandler/ProjectHandler.js
@@ -1,15 +1,51 @@
 import React from "react";
 import MainProject from "./ProjectHandler.style";
 import HeaderRight from "../Header-right/HeaderRight";
+import RepoCard from "../../components/RepoCard/RepoCard";
+import {connect} from "react-redux";
 
-const ProjectHandler = () => {
+const ProjectHandler = ({appState}) => {
+	const [state, setState] = React.useState({
+		repoList: []
+	});
+
+	React.useEffect(() => {
+		fetch(`${appState.helper.urlApi}/users/luctst/repos`)
+			.then(data => data.json())
+			.then(dataJson => setState({repoList: [...dataJson]}))
+			.catch(err => err);
+	}, []);
+
 	return (
 		<>
 			<MainProject>
 				<HeaderRight/>
+				<section>
+					<form>
+						<input type="text" placeholder="Chercher un repository"/>
+					</form>
+				</section>
+				<section>
+					{
+						state.repoList.length === 0 ?
+							<p>Recupération des données..</p>
+						: state.repoList.map(el => {
+							return <RepoCard
+										key={el.id}
+										data={el}
+									/>;
+						})
+					}
+				</section>
 			</MainProject>
 		</>
 	);
 };
 
-export default ProjectHandler;
+const mapStateToProps = state => {
+	return {
+		appState: state
+	}
+}
+
+export default connect(mapStateToProps, null)(ProjectHandler);
--- src/container/ProjectHandler/ProjectHandler.style.js
+++ src/container/ProjectHandler/ProjectHandler.style.js
@@ -1,6 +1,34 @@
 import styled from "styled-components";
 import Main from "../../styles/layout/Main";
 
-const MainProject = styled(Main)``;
+const MainProject = styled(Main)`
+	section:first-of-type {
+		margin-top: 2.5em;
+
+		form {
+			width: 100%;
+
+				input {
+					width: inherit;
+					height: 1.3em;
+					padding: .375rem .75rem;
+					font-size: 1rem;
+					font-weight: 400;
+					line-height: 1.5;
+					color: #495057;
+					background-color: #fff;
+					background-clip: padding-box;
+					border: 1px solid #ced4da;
+					border-radius: .25rem;
+					transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+				}
+		}
+	}
+
+	section {
+		display: flex;
+		flex-wrap: wrap;
+	}
+`;
 
 export default MainProject;
