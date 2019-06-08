927424c9721db38e3153a3c3710d436d8b1c5d9d
Lucas Tostée <lucas.tostee@gmail.com>
2019-06-08 16:00:35 -0400

    Add dataUi data in the appState file for handle ui

--- src/appState.js
+++ src/appState.js
@@ -1,5 +1,22 @@
 export default {
 	helper: {
 		urlApi: "https://api.github.com"
+	},
+	dataUi: {
+		headerLeft: [
+			{ isLinkRouter: true, content: "Projets", link: "projects" },
+			{ isLinkRouter: true, content: "Experiences", link: "experiences" },
+			{ isLinkRouter: false, content: "Twitter", link: "https://www.twitter.com/ltostee"},
+			{ isLinkRouter: false, content: "lucas.tostee@gmail.com", link: "mailto:lucas.tostee@gmail.com"},
+			{ isLinkRouter: false, content: "Actuellement à New-York" }
+		],
+		headerRight: {
+			languageDefault: "Français",
+			languages: [
+				{isActive: true, langue: "Français"},
+				{isActive: false, langue: "English"}
+			],
+			themeDark: true,
+		}
 	}
 }
