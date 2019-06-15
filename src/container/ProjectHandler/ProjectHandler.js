9ff34369cab17dab759d6f22e912a9a56d1c84cd
Lucas Tostée <lucas.tostee@gmail.com>
2019-06-15 03:35:28 -0400

    Update logic for projectHandler and update style

--- src/container/ProjectHandler/ProjectHandler.js
+++ src/container/ProjectHandler/ProjectHandler.js
@@ -1,39 +1,67 @@
 import React from "react";
+import fetchData from "../../utils/fetchData";
 import MainProject from "./ProjectHandler.style";
 import HeaderRight from "../Header-right/HeaderRight";
 import RepoCard from "../../components/RepoCard/RepoCard";
 import {connect} from "react-redux";
+import Chips from "../Chips/Chips";
 
 const ProjectHandler = ({githubUrlApi}) => {
 	const [state, setState] = React.useState({
-		repoList: []
+		repoList: [],
+		modeList: true,
+		repoToFind: ""
 	});
 
 	React.useEffect(() => {
-		fetch(`${githubUrlApi}/users/luctst/repos?per_page=100`)
-			.then(data => data.json())
-			.then(dataJson => setState({repoList: [...dataJson]}))
-			.catch(err => err);
+		(async function() {
+			const newState = {...state};
+			const getReposList = await fetchData(`${githubUrlApi}/users/luctst/repos?per_page=100`);
+			newState.repoList = [...getReposList];
+			setState(newState);
+		})();
 	}, []);
 
-	const handleChange = e => {}
+	const handleChange = e => {
+		const newState = {...state};
+
+		if (e.target.value === "") {
+			newState.modeList = true;
+			setState(newState);
+		} else {
+			newState.modeList = false;
+			newState.repoToFind = e.target.value;
+			setState(newState);
+		}
+	};
+
 
 	return (
 		<MainProject>
 			<HeaderRight/>
 			<section className="wrapper--search--repos">
-				<input type="text" placeholder="Chercher un repository" onChange={handleChange}/>
+				<div className="wrapper--search--repos--find">
+					<input type="text" placeholder="Chercher un repository par titre" onChange={handleChange}/>
+				</div>
+				<div className="wrapper--search--repos--filters">
+					<p>Filtrer par:</p>
+					<Chips icon="fab fa-github" content="Commit" isFilter/>
+					<Chips icon="fas fa-star" content="Stars" isFilter/>
+				</div>
 			</section>
 			<section className="wrapper--list--repos">
 				{
-					state.repoList.length === 0 ?
-						<p>Recupération des données..</p>
-					: state.repoList.map(el => {
-						return <RepoCard
-									key={el.id}
-									data={el}
-								/>;
-					})
+					(() => {
+						if (state.repoList.length === 0) {
+							return <p>Récupération des données.</p>
+						} else if (state.modeList) {
+							return state.repoList.map(el => <RepoCard key={el.id} data={el} />)
+						} else if (!state.modeList) {
+							return state.repoList.map(el => {
+								return el.name.startsWith(state.repoToFind) ? <RepoCard key={el.id} data={el}/> : null
+							});
+						}
+					})()
 				}
 			</section>
 		</MainProject>
--- src/container/ProjectHandler/ProjectHandler.style.js
+++ src/container/ProjectHandler/ProjectHandler.style.js
@@ -5,19 +5,34 @@ const MainProject = styled(Main)`
 	.wrapper--search--repos {
 		margin: 2.5em 0;
 
-		input {
-			width: 100%;
-			height: 1.3em;
-			padding: .375rem .75rem;
-			font-size: 1rem;
-			font-weight: 400;
-			line-height: 1.5;
-			color: #495057;
-			background-color: #fff;
-			background-clip: padding-box;
-			border: 1px solid #ced4da;
-			border-radius: .25rem;
-			transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+		&--find {
+			input {
+				width: 97%;
+				height: 1.3em;
+				padding: .375rem .75rem;
+				font-size: 1rem;
+				font-weight: 400;
+				line-height: 1.5;
+				color: #495057;
+				background-color: #fff;
+				background-clip: padding-box;
+				border: 1px solid #ced4da;
+				border-radius: .25rem;
+				transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+			}
+		}
+
+		&--filters {
+			display: flex;
+			margin-top: 1em;
+			align-items: center;
+
+			p {
+				margin: 0 .5em 0 0;
+			}
+			div {
+				margin-right: 1em;
+			}
 		}
 	}
 
