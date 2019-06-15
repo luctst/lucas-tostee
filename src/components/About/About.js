32b354e78ba93b2fce99ba36fc37cd2babc09e51
Lucas Tostée <lucas.tostee@gmail.com>
2019-06-15 03:33:23 -0400

    Remove content

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,7 @@ const About = () => {
 		<MainAbout>
 			<HeaderRight/>
 			<section>
-				<h1>Tostée Lucas</h1>
+				<h1>Bienvenue</h1>
 				<h2>Welcome banana's fan !! You will find on this website a lot of monkey's, gorilla.. After all they are your ancestors. You can scroll down to learn more about us</h2>
 			</section>
 		</MainAbout>
