43f32cdbe1fef6dd626f8dbb0ede798fbd626ec7
Lucas Tostée <lucas.tostee@gmail.com>
2019-06-18 14:55:59 -0400

    Add rel props for security

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,7 @@ const About = (React.memo(() => {
 		<SCMainAbout>
 			<section>
 				<h1>Bienvenue</h1>
-				<h2>Welcome banana's fan !! You will find on this website a lot of monkey's, gorilla.. After all they are your ancestors. You can scroll down to learn more about us, vous pouvez me suivre sur <a href="https://www.twitter.com/ltostee" target="_blank">Twitter</a> ou sur <a href="https://www.github.com/luctst" target="_blank">GitHub</a></h2>
+				<h2>Welcome banana's fan !! You will find on this website a lot of monkey's, gorilla.. After all they are your ancestors. You can scroll down to learn more about us, vous pouvez me suivre sur <a href="https://www.twitter.com/ltostee" target="_blank" rel="noopener noreferrer">Twitter</a> ou sur <a href="https://www.github.com/luctst" target="_blank" rel="noopener noreferrer">GitHub</a></h2>
 			</section>
 		</SCMainAbout>
 	);
