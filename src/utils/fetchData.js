86f51010900ca54e5a677f744d8e30068b1dc4ce
Lucas Tostée <lucas.tostee@gmail.com>
2019-06-19 16:55:41 -0400

    Add headers object

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -5,7 +5,13 @@
  */
 const fetchData = async url => {
 	try {
-		const data = await fetch(url);
+		const data = await fetch(url, {
+			method: "get",
+			headers: {
+				"Authorization": "token 9233e99c4797333d2871b06ab16b9d30e659a48e",
+				"User-Agent": "luctst",
+			}
+		});
 		const dataParsed = await data.json();
 		return dataParsed;
 	} catch (error) {
