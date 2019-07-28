4a59ba59a765d4c5e2442821ca8ac5fb54c7929c
Lucas Tostée <lucas.tostee@gmail.com>
2019-07-28 20:08:15 +0200

    Remove useless data for appState

--- src/appState.js
+++ src/appState.js
@@ -1,14 +1,6 @@
 export default {
-	helper: {
-		urlApi: "https://api.github.com"
-	},
 	dataUi: {
 		headerRight: {
-			languageDefault: "Français",
-			languages: [
-				{isActive: true, langue: "Français"},
-				{isActive: false, langue: "English"}
-			],
 			themeDark: true,
 		}
 	}
