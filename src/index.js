334d108ad8844253b9b37b47248b0b3ae3adb439
Lucas Tostée <lucas.tostee@gmail.com>
2019-07-28 20:10:50 +0200

    Add all components, routes in the main files for performance

--- src/index.js
+++ src/index.js
@@ -2,12 +2,31 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import store from "./appStore";
-import App from './container/App';
+import { Container } from "./styles/layout/Container";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import HeaderLeft from "./components/HeaderLeft/HeaderLeft";
+import HeaderRight from './container/Header-right/HeaderRight';
+import Loader from './components/Loader/Loader';
 import * as serviceWorker from './serviceWorker';
+const About = React.lazy(() => import("./components/About/About"));
+const ProjectHandler = React.lazy(() => import("./container/ProjectHandler/ProjectHandler"));
+const Notfound = React.lazy(() => import("./components/NotFound/NotFound"));
 
 render(
 	<Provider store={store}>
-		<App/>
+		<Container as="section">
+			<BrowserRouter>
+				<HeaderLeft />
+				<HeaderRight />
+				<React.Suspense fallback={<div><Loader /></div>}>
+					<Switch>
+						<Route exact strict sensitive path="/" component={About} />
+						<Route exact strict sensitive path="/projects" component={ProjectHandler} />
+						<Route component={Notfound} />
+					</Switch>
+				</React.Suspense>
+			</BrowserRouter>
+		</Container>
 	</Provider>
 	,document.getElementById('root')
 );
