3eed62eccb85cf252d6e38bfabf839d397a4fc88
Lucas Tostée <lucas.tostee@gmail.com>
2019-07-28 20:09:52 +0200

    Add logic to get the number of repo created

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,11 +2,30 @@ import React from "react";
 import SCMainAbout from "./About.style";
 
 const About = (React.memo(() => {
+	const [repoLength, setRepoLenght] = React.useState(0);
+
+	React.useEffect(() => {
+		fetch("https://api.github.com/user/repos?page=1&per_page=100", {
+			method: "get",
+			headers: {
+				"Authorization": "token 49aca5506e3cdc96df58adb1ac17d869586ad851",
+				"User-Agent": "luctst",
+			}
+		}).then(response => {
+			if (response.headers.get("link")) {
+			} else {
+				response.json().then(responseParsed => {
+					setRepoLenght(responseParsed.length);
+				});
+			}
+		}).catch(err => new TypeError(err));
+	}, []);
+
 	return (
 		<SCMainAbout>
 			<section>
-				<h1>Bienvenue</h1>
-				<h2>Welcome banana's fan !! You will find on this website a lot of monkey's, gorilla.. After all they are your ancestors. You can scroll down to learn more about us, vous pouvez me suivre sur <a href="https://www.twitter.com/ltostee" target="_blank" rel="noopener noreferrer">Twitter</a> ou sur <a href="https://www.github.com/luctst" target="_blank" rel="noopener noreferrer">GitHub</a></h2>
+				<h1>Welcome !</h1>
+				<h2>I have a more suitable profile for client-side code, but I am now turning to a more versatile profile and training regularly by contributing to open source projects. I love open source and i'll like to be able to earn my life with it, i'm currently manage more than {repoLength !== 0 && repoLength} projects that you can find on my <a href="https://www.github.com/luctst" target="_blank" rel="noopener noreferrer">GitHub</a> and keep me updated about changes on my <a href="https://twitter.com/luctstt" target="_blank" rel="noopener noreferrer">Twitter</a> . Do not hesitate to follow me and / or support me in my diferents projects :)</h2>
 			</section>
 		</SCMainAbout>
 	);
