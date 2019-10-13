b7c0acf47e51b40e7e6e0884aa3cad62e2bbecfd
Lucas Tostée <lucas.tostee@gmail.com>
2019-10-13 22:48:55 +0200

    Update style

--- src/styles/helper.js
+++ src/styles/helper.js
@@ -1,11 +1,5 @@
-const mainColor = "#192735";
-const headerLinksColor = "#d0021b";
-const marginTopHeader = "1em";
-const mainOpacity = .8;
-
 export default {
-	mainColor,
-	headerLinksColor,
-	marginTopHeader,
-	mainOpacity
+	mainColor: "#192735",
+	headerLinksColor: "#d0021b",
+	mainOpacity: .8
 }
