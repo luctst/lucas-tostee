d58ae8d67bd02be242a9608e0a946201b02e068d
Lucas Tostée <lucas.tostee@gmail.com>
2019-10-13 22:40:54 +0200

    Update About component

--- src/components/About/About.style.js
+++ src/components/About/About.style.js
@@ -1,39 +1,52 @@
 import styled from "styled-components";
-import Main from "../../styles/layout/Main";
 import styleUtils from "../../styles/helper";
 
-const SCMainAbout = styled(Main)`
-	display: flex;
-	align-items: center;
+const SCMainAbout = styled.header`
+	height: fit-content;
+	margin: 2em 0;
+	h1 {
+		font-family: 'Jacques Francois', sans-serif;
+		font-size: 64px;
+		margin-top: 0;
+		margin-bottom: 0;
+		color: ${styleUtils.mainColor};
+	}
 
-	section {
-			h1 {
-				font-family: 'Jacques Francois', sans-serif;
-				font-size: 64px;
-				margin-top: 0;
-				margin-bottom: 16px;
-				color: ${styleUtils.mainColor};
-			}
+	h2 {
+		font-family: "Open Sans", sans-serif;
+		font-size: 19px;
+		color: ${styleUtils.mainColor};
+		opacity: ${styleUtils.mainOpacity};
+		padding-right: 4em;
 
-			h2 {
-				font-family: "Open Sans", sans-serif;
-				font-size: 18px;
-				line-height: 30px;
-				color: ${styleUtils.mainColor};
-				opacity: ${styleUtils.mainOpacity};
+		a {
+			color: ${styleUtils.mainColor};
+		}
 
-				a {
-					color: ${styleUtils.mainColor};
-				}
+		a:first-child:hover {
+			color: #00acee;
+		}
 
-				a:first-child:hover {
-					color: #00acee;
-				}
+		a:last-child:hover {
+			color: #AD5C51;
+		}
+	}
 
-				a:last-child:hover {
-					color: #AD5C51;
-				}
-			}
+	@media (max-width: 767px) {
+		h1 {
+			font-size: 52px;
+		}
+		h2 {
+			font-size: 16.5px;
+			line-height: 30px;
+			padding: 0;
+		}
+	}
+
+	@media (min-width: 1000px) {
+		h2 {
+			padding-right: 15em;
+		}
 	}
 `;
 
