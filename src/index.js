96bc5ec5de7105c998cec82d14e1e4ed47014038
Lucas Tostée <lucas.tostee@gmail.com>
2019-10-13 22:43:58 +0200

    Remove useless code

--- src/index.js
+++ src/index.js
@@ -1,29 +1,22 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
-import store from "./appStore";
-import { Container } from "./styles/layout/Container";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import HeaderLeft from "./components/HeaderLeft/HeaderLeft";
+import store from "./state/store/appStore";
+import { Container } from "./styles/Container";
 import Loader from './components/Loader/Loader';
 import * as serviceWorker from './serviceWorker';
-const About = React.lazy(() => import("./components/About/About"));
-const ProjectHandler = React.lazy(() => import("./container/ProjectHandler/ProjectHandler"));
-const Notfound = React.lazy(() => import("./components/NotFound/NotFound"));
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+const Projects = React.lazy(() => import("./container/Projects/Projects"));
 
 render(
 	<Provider store={store}>
-		<Container as="section">
-			<BrowserRouter>
-				<HeaderLeft />
-				<React.Suspense fallback={<div><Loader /></div>}>
-					<Switch>
-						<Route exact strict sensitive path="/" component={About} />
-						<Route exact strict sensitive path="/projects" component={ProjectHandler} />
-						<Route component={Notfound} />
-					</Switch>
-				</React.Suspense>
-			</BrowserRouter>
+		<Container as="main">
+			<React.Suspense fallback={<div><Loader /></div>}>
+				<About/>
+				<Projects/>
+				<Contact/>
+			</React.Suspense>
 		</Container>
 	</Provider>
 	,document.getElementById('root')
