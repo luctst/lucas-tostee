c35dd5eb56c9c6172355f9ccd4b9e070cbd2a6cc
Lucas Tostée <lucas.tostee@gmail.com>
2019-10-13 22:44:20 +0200

    Create Contact component

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SCcontact from "./Contact.style";
+
+const Contact = () => {
+	return (
+		<SCcontact>
+			<summary>Contact</summary>
+			<p>I'm mainly active on <a href="https://www.github.com/luctst">Github</a> and <a href="https://www.twitter.com/@luctstt">Twitter</a> don't hesitate to follow me to stay update 😊</p>
+		</SCcontact>
+	);
+};
+
+export default Contact;
