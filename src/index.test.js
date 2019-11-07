0ba6ea231afc2925f77fa84cee49785d739b1a4d
Lucas Tostée <lucas.tostee@gmail.com>
2019-11-07 09:33:03 +0100

    Update test

--- src/index.test.js
+++ src/index.test.js
@@ -1,34 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import store from "./appStore";
-import { Container } from "./styles/layout/Container";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import HeaderLeft from "./components/HeaderLeft/HeaderLeft";
-import HeaderRight from './container/Header-right/HeaderRight';
+import store from "./state/store/appStore";
+import { Container } from "./styles/Container";
 import Loader from './components/Loader/Loader';
-import * as serviceWorker from './serviceWorker';
-const About = React.lazy(() => import("./components/About/About"));
-const ProjectHandler = React.lazy(() => import("./container/ProjectHandler/ProjectHandler"));
-const Notfound = React.lazy(() => import("./components/NotFound/NotFound"));
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+const Projects = React.lazy(() => import("./container/Projects/Projects"));
 
 
 it("Should render the app without crashing", () => {
 	const divTest = document.createElement("div");
 	render(
 		<Provider store={store}>
-			<Container as="section">
-				<BrowserRouter>
-					<HeaderLeft />
-					<HeaderRight />
-					<React.Suspense fallback={<div><Loader /></div>}>
-						<Switch>
-							<Route exact strict sensitive path="/" component={About} />
-							<Route exact strict sensitive path="/projects" component={ProjectHandler} />
-							<Route component={Notfound} />
-						</Switch>
-					</React.Suspense>
-				</BrowserRouter>
+			<Container as="main">
+				<React.Suspense fallback={<div><Loader /></div>}>
+					<About />
+					<Projects />
+					<Contact />
+				</React.Suspense>
 			</Container>
 		</Provider>
 		, divTest
