c7621ac4a3f02fad541718cb18e8a9b1702fc88d
Lucas Tostée <lucas.tostee@gmail.com>
2019-11-07 09:34:13 +0100

    Create build folder

---
+++ build/precache-manifest.b2599cbdd00df361d8a0c24e2f4fb2cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb83df46791402e529e36013989eb972",
+    "url": "/index.html"
+  },
+  {
+    "revision": "023070c1ab005533f2cf",
+    "url": "/static/js/2.6cc7eee6.chunk.js"
+  },
+  {
+    "revision": "adf87a8292c990199b35",
+    "url": "/static/js/3.da84331f.chunk.js"
+  },
+  {
+    "revision": "905db410b74ba95ec8d5",
+    "url": "/static/js/main.1e219f59.chunk.js"
+  },
+  {
+    "revision": "2698770a57a638286b9b",
+    "url": "/static/js/runtime-main.1d5c1715.js"
+  }
+]);
