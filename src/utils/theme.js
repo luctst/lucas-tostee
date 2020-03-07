3ef878d9f2c84a091d27fbb961d4ce004bb7c13f
Lucas Tostée <lucas.tostee@gmail.com>
2020-03-07 22:43:47 +0100

    Add dark and light properties

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,5 +1,10 @@
 export default {
-	mainColor: "#192735",
+	light: {
+		colorContent: "#192735"
+	},
+	dark: {
+		colorContent: "#f3f3f3f3"
+	},
 	headerLinksColor: "#d0021b",
 	mainOpacity: 0.8
 };
