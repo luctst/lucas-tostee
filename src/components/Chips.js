bc66bcde98d422c406ede8894767301e4b36c4e8
Lucas Tostée <lucas.tostee@gmail.com>
2020-03-07 17:25:18 +0100

    Move components in the components folder

---
+++ src/components/Chips.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ChipsWrapper from "../styles/Chips.style";
+
+/**
+ * The Chips component is helpful to display Additional information's.
+ * @param {String } content The content of your Chips can be a string or a number.
+ */
+const Chips = props => {
+	return (
+		<ChipsWrapper contentType={props.content}>
+			{props.content === null ? "Markdown" : props.content}
+		</ChipsWrapper>
+	);
+};
+
+export default Chips;
---
+++ src/components/Projects.js
@@ -0,0 +1,51 @@
+import React from "react";
+import SCprojects from "../styles/Projects.style";
+import talkToApi from "../utils/talkToApi";
+import Chips from "./Chips";
+
+const Projects = () => {
+	const [state, setState] = React.useState({
+		dataFetched: false,
+		data: []
+	});
+
+	React.useEffect(() => {
+		talkToApi("/user/repos?sort=pushed&per_page=10", "get", {
+			headers: {
+				Authorization: `token ${process.env.REACT_APP_APIKEY}`,
+ 				"User-Agent": "luctst"
+			}
+		})
+		.then(response => setState({dataFetched: !state.dataFetched, data: [...response]}));
+	}, []);
+
+	return (
+		<SCprojects>
+			<summary>Projects</summary>
+			<ul>
+				{state.dataFetched !== false
+					? state.data.map(item => (
+							<li key={item.id}>
+								<a href={item.html_url} target="_blank" rel="noopener noreferrer">
+									{item.name}
+								</a>
+								<Chips content={item.language} />
+							</li>
+					  ))
+					: null}
+			</ul>
+			<p>
+				To check all my activity{" "}
+				<a
+					href="https://gitstalk.netlify.com/luctst"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					click here
+				</a>
+			</p>
+		</SCprojects>
+	);
+};
+
+export default Projects;
